51c3c304ac527dc773740285b8dd5215cc7cf279
fantasysk <lisankai93@qq.com>
2026-01-13 16:21:50 +0800

    fix: compatibility for 'fail' vs 'failed' state in API filters

--- applications/task/filters.py
+++ applications/task/filters.py
@@ -2,4 +2,9 @@ import django_filters
 
 
 class TaskFilters(django_filters.FilterSet):
-    state = django_filters.CharFilter(lookup_expr="iexact")
+    state = django_filters.CharFilter(method='filter_state')
+
+    def filter_state(self, queryset, name, value):
+        if value in ['fail', 'failed']:
+            return queryset.filter(state__in=['fail', 'failed'])
+        return queryset.filter(state__iexact=value)
